257913f214dcf5067e8db3f788e2b8035c2f75be
pavel <zavgpavel@yandex.ru>
2022-05-04 22:31:33 +0300

    4may

--- js/script.js
+++ js/script.js
@@ -239,7 +239,7 @@ function availableCurr(arr, missingCurr) {
 availableCurr ([...baseCurrencies], [...additionalCurrencies]); 
 console.log (availableCurr ([...baseCurrencies], [...additionalCurrencies]), 'CNY'); */
 
-function foo(a,b) {
+/* function foo(a,b) {
     const [first] = a;
     const {eng} = b;
  
@@ -247,4 +247,58 @@ function foo(a,b) {
 }
  
 const result = foo(['Hello', 'Привет'], {ru: 'Мир', eng: 'World'});
-console.log (result);
+console.log (result); */
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
+
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Открыто' : answer = 'Закрыто';
+
+    return answer;
+}
+
+console.log(isOpen(restorantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < (+average.slice(0, -1))) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors = {name: 'Mike', age: 32};
+    return copy;
+}
+
+transferWaitors(restorantData);
+console.log(transferWaitors(restorantData));
